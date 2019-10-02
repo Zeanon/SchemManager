af999e8f1ac2f2b5dcf59170f6cf4ecbe1f8d127
Zeanon <thezeanon@gmail.com>
2019-10-02 05:10:56 +0200

    Update

--- src/de/zeanon/schemmanager/worldeditversion/listener/TabCompleter.java
+++ src/de/zeanon/schemmanager/worldeditversion/listener/TabCompleter.java
@@ -1,11 +1,5 @@
 package de.zeanon.schemmanager.worldeditversion.listener;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-
-import java.util.Arrays;
-import java.util.List;
-
 public class TabCompleter {
 
 
