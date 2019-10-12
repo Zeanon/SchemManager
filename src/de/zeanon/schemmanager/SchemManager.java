250f0c0cc0f13a49b0975ca11d5312246ef69aad
Zeanon <thezeanon@gmail.com>
2019-10-12 02:59:35 +0200

    Internal refactor

--- src/de/zeanon/schemmanager/SchemManager.java
+++ src/de/zeanon/schemmanager/SchemManager.java
@@ -48,7 +48,6 @@ public class SchemManager extends JavaPlugin {
             System.out.println("[" + getName() + "] >> Loading Configs.");
             try {
                 config = new Config("config", getDataFolder().getAbsolutePath(), "resources/config");
-                config.insertTestKey();
                 System.out.println("[" + getName() + "] >> [Configs] >> " + config.getName() + " loaded.");
             } catch (Exception e) {
                 e.printStackTrace();
