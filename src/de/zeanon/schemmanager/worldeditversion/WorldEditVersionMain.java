d8814f89adfe72b8ebbf74ff1316c0e059badf63
Zeanon <thezeanon@gmail.com>
2019-10-15 21:54:56 +0200

    Fixed Version

--- src/de/zeanon/schemmanager/worldeditversion/WorldEditVersionMain.java
+++ src/de/zeanon/schemmanager/worldeditversion/WorldEditVersionMain.java
@@ -14,7 +14,6 @@ public class WorldEditVersionMain {
     public static Config weConfig;
 
     public static void onEnable() {
-        SchemManager.config.set("this.is.a.test", false);
         try {
             weConfig = new Config("config", Objects.requireNonNull(SchemManager.getPluginManager().getPlugin("WorldEdit")).getDataFolder().getAbsolutePath());
             System.out.println("[" + SchemManager.getInstance().getName() + "] >> WorldEdit Config is loaded sucessfully.");
