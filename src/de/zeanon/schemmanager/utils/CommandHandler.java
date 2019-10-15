1e8c8d5e2680b0d3f7531ec54e17771790dd6929
Zeanon <thezeanon@gmail.com>
2019-10-15 14:38:10 +0200

    Internal Refactor

--- src/de/zeanon/schemmanager/utils/CommandHandler.java
+++ src/de/zeanon/schemmanager/utils/CommandHandler.java
@@ -6,6 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class CommandHandler implements CommandExecutor {
 
@@ -15,94 +16,82 @@ public class CommandHandler implements CommandExecutor {
     @SuppressWarnings("NullableProblems")
     @Override
     public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
-        if (command.getName().equalsIgnoreCase("schemmanager")) {
-            if (sender instanceof Player) {
-                Player p = (Player) sender;
-                if (args.length == 0) {
-                    p.sendMessage(ChatColor.RED + "Missing argument for " + ChatColor.YELLOW + "<" + ChatColor.GOLD
-                            + "argument" + ChatColor.YELLOW + ">");
-                    return sendUpdateUsage(p) && sendDisableUsage(p);
-                } else if (args[0].equalsIgnoreCase("disable") && p.hasPermission("schemmanager.disable")) {
-                    if (args.length == 1) {
-                        MessageUtils.sendBooleanMessage(ChatColor.RED + "Do you really want to disable " + ChatColor.DARK_PURPLE
-                                        + SchemManager.getInstance().getName() + ChatColor.RED + "? ", "/schemmanager disable confirm", "/schemmanager disable deny",
-                                p);
-                        RequestUtils.addDisableRequest(p);
-                        return true;
-                    } else if (args.length == 2 && (args[1].equalsIgnoreCase("confirm") || args[1].equalsIgnoreCase("deny"))) {
-                        if (args[1].equalsIgnoreCase("confirm") && RequestUtils.checkDisableRequest(p)) {
-                            p.sendMessage(ChatColor.DARK_PURPLE + SchemManager.getInstance().getName() + ChatColor.RED + " is being disabled.");
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (command.getName().equalsIgnoreCase("schemmanager")) {
+                    if (sender instanceof Player) {
+                        Player p = (Player) sender;
+                        if (args.length == 0) {
+                            p.sendMessage(ChatColor.RED + "Missing argument for " + ChatColor.YELLOW + "<" + ChatColor.GOLD
+                                    + "argument" + ChatColor.YELLOW + ">");
+                            sendUpdateUsage(p);
+                            sendDisableUsage(p);
+                        } else if (args[0].equalsIgnoreCase("disable") && p.hasPermission("schemmanager.disable")) {
+                            if (args.length == 1) {
+                                MessageUtils.sendBooleanMessage(ChatColor.RED + "Do you really want to disable " + ChatColor.DARK_PURPLE
+                                                + SchemManager.getInstance().getName() + ChatColor.RED + "? ", "/schemmanager disable confirm", "/schemmanager disable deny",
+                                        p);
+                                RequestUtils.addDisableRequest(p);
+                            } else if (args.length == 2 && (args[1].equalsIgnoreCase("confirm") || args[1].equalsIgnoreCase("deny"))) {
+                                if (args[1].equalsIgnoreCase("confirm") && RequestUtils.checkDisableRequest(p)) {
+                                    p.sendMessage(ChatColor.DARK_PURPLE + SchemManager.getInstance().getName() + ChatColor.RED + " is being disabled.");
+                                    SchemManager.getPluginManager().disablePlugin(SchemManager.getInstance());
+                                } else if (args[1].equalsIgnoreCase("deny") && RequestUtils.checkDisableRequest(p)) {
+                                    RequestUtils.removeDisableRequest(p);
+                                    p.sendMessage(
+                                            ChatColor.DARK_PURPLE + SchemManager.getInstance().getName() + ChatColor.RED + " will not be disabled.");
+                                }
+                            } else {
+                                p.sendMessage(ChatColor.RED + "Too many arguments.");
+                                sendDisableUsage(p);
+                            }
+                        } else if (args[0].equalsIgnoreCase("update") && p.hasPermission("schemmanager.update")) {
+                            if (args.length == 1) {
+                                MessageUtils.sendBooleanMessage(ChatColor.RED + "Do you really want to update?", "/schemmanager update confirm",
+                                        "/schemmanager update deny", p);
+                                RequestUtils.addUpdateRequest(p);
+                            } else if (args.length == 2 && (args[1].equalsIgnoreCase("confirm") || args[1].equalsIgnoreCase("deny"))) {
+                                if (args[1].equalsIgnoreCase("confirm") && RequestUtils.checkUpdateRequest(p)) {
+                                    RequestUtils.removeUpdateRequest(p);
+                                    Update.updatePlugin(p);
+                                } else if (args[1].equalsIgnoreCase("deny") && RequestUtils.checkUpdateRequest(p)) {
+                                    RequestUtils.removeUpdateRequest(p);
+                                    p.sendMessage(ChatColor.DARK_PURPLE + SchemManager.getInstance().getName() + ChatColor.RED + " will not be updated.");
+                                }
+                            } else {
+                                p.sendMessage(ChatColor.RED + "Too many arguments.");
+                                sendUpdateUsage(p);
+                            }
+                        } else {
+                            p.sendMessage(ChatColor.RED + "Invalid sub-command '" + ChatColor.GOLD + "" + args[0] + ChatColor.RED + ".");
+                            sendUpdateUsage(p);
+                            sendDisableUsage(p);
+                        }
+                    } else {
+                        if (args.length == 1 && args[0].equalsIgnoreCase("disable")) {
                             SchemManager.getPluginManager().disablePlugin(SchemManager.getInstance());
-                            return true;
-                        } else if (args[1].equalsIgnoreCase("deny") && RequestUtils.checkDisableRequest(p)) {
-                            RequestUtils.removeDisableRequest(p);
-                            p.sendMessage(
-                                    ChatColor.DARK_PURPLE + SchemManager.getInstance().getName() + ChatColor.RED + " will not be disabled.");
-                            return true;
-                        } else {
-                            return true;
+                        } else if (args[0].equalsIgnoreCase("update")) {
+                            Update.updatePlugin();
                         }
-                    } else {
-                        p.sendMessage(ChatColor.RED + "Too many arguments.");
-                        return sendDisableUsage(p);
                     }
-                } else if (args[0].equalsIgnoreCase("update") && p.hasPermission("schemmanager.update")) {
-                    if (args.length == 1) {
-                        MessageUtils.sendBooleanMessage(ChatColor.RED + "Do you really want to update?", "/schemmanager update confirm",
-                                "/schemmanager update deny", p);
-                        RequestUtils.addUpdateRequest(p);
-                        return true;
-                    } else if (args.length == 2 && (args[1].equalsIgnoreCase("confirm") || args[1].equalsIgnoreCase("deny"))) {
-                        if (args[1].equalsIgnoreCase("confirm") && RequestUtils.checkUpdateRequest(p)) {
-                            RequestUtils.removeUpdateRequest(p);
-                            return Update.updatePlugin(p);
-                        } else if (args[1].equalsIgnoreCase("deny") && RequestUtils.checkUpdateRequest(p)) {
-                            RequestUtils.removeUpdateRequest(p);
-                            p.sendMessage(ChatColor.DARK_PURPLE + SchemManager.getInstance().getName() + ChatColor.RED + " will not be updated.");
-                            return true;
-                        } else {
-                            return true;
-                        }
-                    } else {
-                        p.sendMessage(ChatColor.RED + "Too many arguments.");
-                        return sendUpdateUsage(p);
-                    }
-                } else {
-                    p.sendMessage(ChatColor.RED + "Invalid sub-command '" + ChatColor.GOLD + "" + args[0] + ChatColor.RED + ".");
-                    return sendUpdateUsage(p) && sendDisableUsage(p);
-                }
-            } else {
-                if (args.length == 1 && args[0].equalsIgnoreCase("disable")) {
-                    SchemManager.getPluginManager().disablePlugin(SchemManager.getInstance());
-                    return true;
-                } else if (args[0].equalsIgnoreCase("update")) {
-                    return Update.updatePlugin();
-                } else {
-                    return false;
                 }
             }
-        } else {
-            return false;
-        }
+        }.runTaskAsynchronously(SchemManager.getInstance());
+        return true;
     }
 
-    /**
-     * @param p Player to send to
-     * @return usage
-     */
-    private boolean sendUpdateUsage(final Player p) {
+    private void sendUpdateUsage(final Player p) {
         MessageUtils.sendSuggestMessage(ChatColor.RED + "Usage: ",
                 ChatColor.GRAY + "/schemmanager" + ChatColor.AQUA + " update", ChatColor.DARK_GREEN + ""
                         + ChatColor.UNDERLINE + "" + ChatColor.ITALIC + "" + ChatColor.BOLD + "!!UPDATE BABY!!",
                 "/schemmanager update", p);
-        return true;
     }
 
-    private boolean sendDisableUsage(final Player p) {
+    private void sendDisableUsage(final Player p) {
         MessageUtils.sendSuggestMessage(ChatColor.RED + "Usage: ",
                 ChatColor.GRAY + "/schemmanager" + ChatColor.AQUA + " disable", ChatColor.DARK_RED + ""
                         + ChatColor.UNDERLINE + "" + ChatColor.ITALIC + "" + ChatColor.BOLD + "PLS DON'T D;",
                 "/schemmanager disable", p);
-        return true;
     }
 }
--- src/de/zeanon/schemmanager/worldeditversion/listener/EventListener.java
+++ src/de/zeanon/schemmanager/worldeditversion/listener/EventListener.java
@@ -12,6 +12,7 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.event.server.TabCompleteEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -28,33 +29,38 @@ public class EventListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH)
     public void onTab(final TabCompleteEvent event) {
-        String message = event.getBuffer();
-        while (message.contains("  ")) {
-            message = message.replaceAll(" {2}", " ");
-        }
-        String[] args = message.replaceAll("worldedit:", "/").split(" ");
-        if (args[0].equalsIgnoreCase("//schem") || args[0].equalsIgnoreCase("//schematic")) {
-            if (message.contains("./")) {
-                event.setCompletions(new ArrayList<>());
-            } else {
-                boolean deep = false;
-                if (args.length > 2 && args[2].equalsIgnoreCase("-deep")) {
-                    args = (String[]) ArrayUtils.removeElement(args, "-deep");
-                    deep = true;
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                String message = event.getBuffer();
+                while (message.contains("  ")) {
+                    message = message.replaceAll(" {2}", " ");
                 }
-                if (args.length > 2 && args[2].equalsIgnoreCase("-d")) {
-                    args = (String[]) ArrayUtils.removeElement(args, "-d");
-                    deep = true;
+                String[] args = message.replaceAll("worldedit:", "/").split(" ");
+                if (args[0].equalsIgnoreCase("//schem") || args[0].equalsIgnoreCase("//schematic")) {
+                    if (message.contains("./")) {
+                        event.setCompletions(new ArrayList<>());
+                    } else {
+                        boolean deep = false;
+                        if (args.length > 2 && args[2].equalsIgnoreCase("-deep")) {
+                            args = (String[]) ArrayUtils.removeElement(args, "-deep");
+                            deep = true;
+                        }
+                        if (args.length > 2 && args[2].equalsIgnoreCase("-d")) {
+                            args = (String[]) ArrayUtils.removeElement(args, "-d");
+                            deep = true;
+                        }
+                        event.setCompletions(WorldEditVersionTabCompleter.onTab(args, event.getBuffer(), deep, message.endsWith(" ")));
+                    }
+                } else if (args[0].equalsIgnoreCase("/stoplag")) {
+                    if (args.length == 1 || (args.length == 2 && !message.endsWith(" "))) {
+                        event.setCompletions(Collections.singletonList("-c"));
+                    } else {
+                        event.setCompletions(new ArrayList<>());
+                    }
                 }
-                event.setCompletions(WorldEditVersionTabCompleter.onTab(args, event.getBuffer(), deep, message.endsWith(" ")));
             }
-        } else if (args[0].equalsIgnoreCase("/stoplag")) {
-            if (args.length == 1 || (args.length == 2 && !message.endsWith(" "))) {
-                event.setCompletions(Collections.singletonList("-c"));
-            } else {
-                event.setCompletions(new ArrayList<>());
-            }
-        }
+        }.runTaskAsynchronously(SchemManager.getInstance());
     }
 
 
