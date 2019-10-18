51969649e5d1afc21c1de9c0fa8957f99fcef69c
Zeanon <thezeanon@gmail.com>
2019-10-18 12:52:53 +0200

    Reformatted Code internally

--- src/de/zeanon/schemmanager/SchemManager.java
+++ src/de/zeanon/schemmanager/SchemManager.java
@@ -46,6 +46,7 @@ public class SchemManager extends JavaPlugin {
 				System.out.println("[" + getName() + "] >> [Configs] >> " + config.getName() + " could not be loaded");
 				failedToLoad = true;
 			}
+
 			if (failedToLoad) {
 				System.out.println("[" + getName() + "] >> Could not load config files... unloading Plugin...");
 				SchemManager.getPluginManager().disablePlugin(SchemManager.getInstance());
